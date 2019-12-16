0841b02eb795179e33727bdd197e2eced872b53a
2101975298@qq.com <hzl1216>
2019-12-16 10:35:06 +0800

    add supervised

--- tcga.py
+++ tcga.py
@@ -86,34 +86,27 @@ def main():
     for epoch in range(args.start_epoch, args.epochs):
         start_time = time.time()
         # train for one epoch
-        class_loss, cons_loss = train_semi(train_labeled_loader, train_unlabeled_loader, model, ema_model, optimizer,ema_optimizer, epoch,scheduler)
+        if args.semi:
+            print (' train in semi-supervised')
+            class_loss, cons_loss = train_semi(train_labeled_loader, train_unlabeled_loader, model, ema_model, optimizer,ema_optimizer, epoch,scheduler)
+        else:
+            print (' train in supervised')
+            class_loss = train(train_labeled_loader, model, ema_model, optimizer, ema_optimizer, epoch,scheduler)
+            cons_loss =0
         print("--- training epoch in %s seconds ---" % (time.time() - start_time))
 
         if args.evaluation_epochs and (epoch + 1) % args.evaluation_epochs == 0:
-            if args.semi:
-                print (' train in semi-supervised')
-                start_time = time.time()
-                print("Evaluating the  model:")
-                val_loss, val_acc = validate(eval_loader, model, criterion,epoch)
-                print("--- validation in %s seconds ---" % (time.time() - start_time))
-                logger.append([epoch, class_loss, cons_loss, val_loss, val_acc])
+            start_time = time.time()
+            print("Evaluating the  model:")
+            val_loss, val_acc = validate(eval_loader, model, criterion,epoch)
+            print("--- validation in %s seconds ---" % (time.time() - start_time))
+            logger.append([epoch, class_loss, cons_loss, val_loss, val_acc])
 
-                print("Evaluating the EMA model:")
-                ema_val_loss, ema_val_acc = validate(eval_loader, ema_model, criterion,epoch)
-                print("--- validation in %s seconds ---" % (time.time() - start_time))
-                logger.append([epoch, class_loss, cons_loss, ema_val_loss, ema_val_acc])
-            else:
-                print (' train in supervised')
-                start_time = time.time()
-                print("Evaluating the  model:")
-                val_loss, val_acc = validate(eval_loader, model, criterion, epoch)
-                print("--- validation in %s seconds ---" % (time.time() - start_time))
-                logger.append([epoch, class_loss, 0, val_loss, val_acc])
+            print("Evaluating the EMA model:")
+            ema_val_loss, ema_val_acc = validate(eval_loader, ema_model, criterion,epoch)
+            print("--- validation in %s seconds ---" % (time.time() - start_time))
+            logger.append([epoch, class_loss, cons_loss, ema_val_loss, ema_val_acc])
 
-                print("Evaluating the EMA model:")
-                ema_val_loss, ema_val_acc = validate(eval_loader, ema_model, criterion, epoch)
-                print("--- validation in %s seconds ---" % (time.time() - start_time))
-                logger.append([epoch, class_loss, 0, ema_val_loss, ema_val_acc])
         if args.checkpoint_epochs and (epoch + 1) % args.checkpoint_epochs == 0:
             save_checkpoint({
                 'epoch': epoch + 1,
